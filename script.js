0626c940a6fb3e207ab22b1b60adacd8ec5140c1
josedalprafonseca-gif <jose.dalpra.fonseca@escola.pr.gov.br>
2026-08-12 12:10:11 -0300

    Update script.js  2

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-    <script>
-        function openTab(evt, recipeId) {
-            var i, content, tablinks;
-            content = document.getElementsByClassName("recipe-content");
-            for (i = 0; i < content.length; i++) {
-                content[i].classList.remove("active");
-            }
-            tablinks = document.getElementsByClassName("tab-btn");
-            for (i = 0; i < tablinks.length; i++) {
-                tablinks[i].classList.remove("active");
-            }
-            document.getElementById(recipeId).classList.add("active");
-            evt.currentTarget.classList.add("active");
-        }
-    </script>
+function openTab(evt, recipeId) {
+    var i, content, tablinks;
 
-</body>
-</html>
+    // Esconde todo o conteúdo das receitas
+    content = document.getElementsByClassName("recipe-content");
+    for (i = 0; i < content.length; i++) {
+        content[i].classList.remove("active");
+    }
+
+    // Remove o destaque (classe active) de todos os botões
+    tablinks = document.getElementsByClassName("tab-btn");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+
+    // Exibe a receita selecionada e destaca o botão clicado
+    document.getElementById(recipeId).classList.add("active");
+    evt.currentTarget.classList.add("active");
+}
